8fab2d8af6647ca42a7656ede6253b30053a449f
Kieran Levin <ktl@frame.work>
2025-09-05 06:55:37 -0700

    update comments for generic pcie device
    This is updating comments for a generic PCIE device, repurposing one
    GPIO for a 12V regulator enable pin, and the other to power sequence the
    3.3V PCIE power enable with system power on, to allow generic PCIE
    devices to be used.
    
    Signed-off-by: Kieran Levin <ktl@frame.work>

--- pcie.c
+++ pcie.c
@@ -10,7 +10,7 @@ struct default_ssd_cfg pcie_accessory_cfg = {
 		.hardware_version = 0x0008,
 		.hardware_revision = 0,
 		.serial = {'F', 'R', 'A', 'P', 'C', 'I', 'E', '0', '0', '0',
-					'0', '0', '0', 'T', 'E', 'S', 'T', '1', '\0', '\0'},
+					'0', '0', '0', 'T', 'E', 'S', 'T', '8', '\0', '\0'},
 		.descriptor_length = sizeof(struct default_ssd_cfg) - sizeof(struct gpu_cfg_descriptor),
 		.descriptor_crc32 = 0,
 		.crc32 = 0
@@ -27,15 +27,15 @@ struct default_ssd_cfg pcie_accessory_cfg = {
 	.hdr3 = {.block_type = GPUCFG_TYPE_VENDOR, .block_length = sizeof(enum gpu_vendor)},
 	.vendor = GPU_PCIE_ACCESSORY,
 
-	/* Power enable for SSD1 */
+	/* Power enable for 12V */
 	.hdr4 = {.block_type = GPUCFG_TYPE_GPIO, .block_length = sizeof(struct gpu_cfg_gpio) * 8},
 	.gpio0 = {.gpio = GPU_1G1_GPIO0_EC, .function = GPIO_FUNC_HIGH, .flags = GPIO_OUTPUT_LOW, .power_domain = POWER_S3},
-	/* Power enable for SSD2 */
+	/* Power enable for 3.3V */
 	.gpio1 = {.gpio = GPU_1H1_GPIO1_EC, .function = GPIO_FUNC_HIGH, .flags = GPIO_OUTPUT_LOW, .power_domain = POWER_S3},
 	/* UNUSED */
-	.gpio2 = {.gpio = GPU_2A2_GPIO2_EC, .function = GPIO_FUNC_UNUSED, .flags = GPIO_OUTPUT_LOW, .power_domain = POWER_G3},
+	.gpio2 = {.gpio = GPU_2A2_GPIO2_EC, .function = GPIO_FUNC_UNUSED, .flags = GPIO_INPUT, .power_domain = POWER_G3},
 	/* UNUSED */
-	.gpio3 = {.gpio = GPU_2L7_GPIO3_EC, .function = GPIO_FUNC_UNUSED, .flags = GPIO_OUTPUT_LOW, .power_domain = POWER_G3},
+	.gpio3 = {.gpio = GPU_2L7_GPIO3_EC, .function = GPIO_FUNC_UNUSED, .flags = GPIO_INPUT, .power_domain = POWER_G3},
 	/* set mux configuration on mainboard for SSD */
 	.gpio_edpaux = {.gpio = GPU_PCIE_MUX_SEL, .function = GPIO_FUNC_HIGH, .flags = GPIO_OUTPUT_LOW, .power_domain = POWER_S3},
 	/* GPU_VSYS_EN */
