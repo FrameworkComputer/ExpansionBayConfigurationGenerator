ee2e37c44fd56b2b32e1c70a504b3dfe87f8196d
Kieran Levin <ktl@frame.work>
2026-02-02 08:07:07 -0800

    add GPU fan flag to keep the fan always on.
    This flag will always keep the fan on at the min_rpm value.
    
    Signed-off-by: Kieran Levin <ktl@frame.work>

--- config_definition.h
+++ config_definition.h
@@ -167,6 +167,9 @@ struct gpu_cfg_custom_temp {
 	uint16_t temp_fan_max;
 } __packed;
 
+enum gpu_cfg_fan_flag {
+	GPU_CFG_FAN_FLAG_ALWAYS_ON = 0x01,
+};
 struct gpu_cfg_fan {
 	uint8_t idx;
 	uint8_t flags;
